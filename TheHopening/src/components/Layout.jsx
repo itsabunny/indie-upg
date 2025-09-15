fc09a55bda65b3aedf3516181b03bda727290a02
itsabunny <itsabunny@pm.me>
2025-09-15 19:44:58 +0200

    Add Layout component for consistent page structure and styling

---
+++ TheHopening/src/components/Layout.jsx
@@ -0,0 +1,22 @@
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography variant="h6" component="div">Nyhetssidan</Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ py: 3 }}>
+        <Container maxWidth="md">
+          {children}
+        </Container>
+      </Box>
+    </>
+  );
+}
