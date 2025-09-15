ba392181860e70a9aec0597e3837c03076eca34d
itsabunny <itsabunny@pm.me>
2025-09-15 19:43:06 +0200

    Refactor App component to include routing and layout structure

--- TheHopening/src/App.jsx
+++ TheHopening/src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import ArticleDetail from './pages/ArticleDetail';
 
-function App() {
+export default function App() {
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Toaster position="top-right" />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/article/:id" element={<ArticleDetail />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
-
-export default App
